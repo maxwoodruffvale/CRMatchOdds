a324945cef929e4a1275e3209f2fcad0082f2fa3
Max Vale <maxwellvale@icloud.com>
2025-01-07 15:23:28 -0800

    cookies; write to file as searching

--- app.py
+++ app.py
@@ -1,16 +1,42 @@
 import flask as fk
-from flask import render_template, redirect, url_for, session, request
+from flask import render_template, redirect, url_for, session, request, make_response
 import logging
+from interact import get_recent_matches_and_odds
+import math
+
+import sys
+print(sys.executable)
 
 logging.basicConfig(level=logging.DEBUG)
 app = fk.Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
 def index():
+    previous_tag = request.cookies.get('tag')
+
+
     if(fk.request.method == "GET"):
-        return render_template("home.html")
+        if(previous_tag):
+            matches = get_recent_matches_and_odds("#" + previous_tag)
+            return render_template("home.html", match_data = matches)
+        else:
+            return render_template("home.html")
     else:
-        return render_tempalte()
+        tag = request.form.get('tag')
+        save = request.form.get('save')
+        matches = get_recent_matches_and_odds("#" + tag)
+        response = make_response(render_template('home.html', match_data=matches))
+        if(save=="on"):
+            response.set_cookie('tag', tag)
+            print("save it")
+        
+        return(response)
+        #return render_template("home.html", match_data = matches)
+
+        #        response = make_response(render_template_string(template, previous_input=user_input))
+        #         response.set_cookie('user_input', user_input)
+        #    previous_input = request.cookies.get('user_input')
+
 
 @app.route("/about", methods=["GET"])
 def about():
@@ -20,6 +46,9 @@ def about():
 def custom_match():
     return "Custom match otw"
 
+@app.template_filter('floor')
+def floor_filter(value):
+    return math.floor(value)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
--- gatherMatches.py
+++ gatherMatches.py
@@ -54,45 +54,49 @@ def extract_matches(limit):
     with requests.Session() as session:
         session.headers.update({"Authorization": f"Bearer {API_TOKEN}"})
 
-        while(player_queue):
-            player = player_queue.pop(0)
-            if player in processed_players:
-                continue
-            processed_players.add(player)
+        output_file = "matches.csv"
+        with open(output_file, mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            header = ['Winner'] + [f'P{i+1}' for i in range(8)] + [f'O{i+1}' for i in range(8)]
+            writer.writerow(header)
 
-            battle_log = get_battle_log(session, player)
-            for battle in battle_log:
-                if battle.get('type') != "pathOfLegend":
-                    continue
-
-                match_data = {
-                    "winner": int(battle["team"][0]["crowns"] < battle["opponent"][0]["crowns"]),
-                    "player_deck": [card_to_num(card["name"]) for card in battle["team"][0]["cards"]],
-                    "opponent_deck": [card_to_num(card["name"]) for card in battle["opponent"][0]["cards"]],
-                }
-                # first winner is player, 0 = player win, 1 = opponent win
-                #print(battle["team"][0]["cards"][0]) <-- HAS LINKS TO IMAGES FOR FUTURE REFERENCE
-
-                matches.append(match_data)
-
-                if(len(matches) % 100 == 0): print(f'{len(matches):10}')
+            while(player_queue):
                 if(len(matches) >= limit):
-                    return matches
+                        break
+                
+                player = player_queue.pop(0)
+                if player in processed_players:
+                    continue
+                processed_players.add(player)
 
-                player_queue.append(battle["opponent"][0].get("tag"))
-    
-    output_file = "matches.csv"
-    with open(output_file, mode='w', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        header = ['Winner'] + [f'P{i+1}' for i in range(8)] + [f'O{i+1}' for i in range(8)]
-        writer.writerow(header)
+                battle_log = get_battle_log(session, player)
+                for battle in battle_log:
+                    if battle.get('type') != "pathOfLegend":
+                        continue
 
-        for match in matches:
-            player_deck = match['player_deck']
-            player_deck.sort()
-            opponent_deck = match['opponent_deck']
-            opponent_deck.sort()
-            row = [match['winner']] + player_deck + opponent_deck
-            writer.writerow(row)
+                    match_data = {
+                        "winner": int(battle["team"][0]["crowns"] < battle["opponent"][0]["crowns"]),
+                        "player_deck": [card_to_num(card["name"]) for card in battle["team"][0]["cards"]],
+                        "opponent_deck": [card_to_num(card["name"]) for card in battle["opponent"][0]["cards"]],
+                    }
 
+                    # first winner is player, 0 = player win, 1 = opponent win
+                    #print(battle["team"][0]["cards"][0]) #<-- HAS LINKS TO IMAGES FOR FUTURE REFERENCE
+
+                    matches.append(match_data)
+
+                    if(len(matches) % 100 == 0): print(f'{len(matches):10}')
+                    if(len(matches) >= limit):
+                        break
+
+                    player_queue.append(battle["opponent"][0].get("tag"))
+                    
+                    player_deck = match_data['player_deck']
+                    player_deck.sort()
+                    opponent_deck = match_data['opponent_deck']
+                    opponent_deck.sort()
+                    row = [match_data['winner']] + player_deck + opponent_deck
+
+                    writer.writerow(row)
     return matches
+#131.215.220.164
