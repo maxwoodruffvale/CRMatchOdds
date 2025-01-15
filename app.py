f7ce9cb9118e91acfea336fcd0d82b5613e5c652
Max Vale <maxwellvale@icloud.com>
2025-01-14 23:44:08 -0800

    added custom match functionality

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import flask as fk
 from flask import render_template, redirect, url_for, session, request, make_response
-from interactRF import get_recent_matches_and_odds, get_recent_matches_key
+from interactRF import get_recent_matches_and_odds, get_recent_matches_key, predict_team_odds
 import math
 
 import sys
@@ -37,7 +37,30 @@ def about():
 
 @app.route("/custom-match", methods=["GET", "POST"])
 def custom_match():
-    return "Custom matches coming soon"
+    if(fk.request.method == "GET"):
+        with open("card_imgs.csv", "r") as file:
+            card_imgs = file.readlines()
+        card_imgs = [line.strip().split(',') for line in card_imgs]
+        return render_template("custommatch.html", card_imgs=card_imgs)
+    else:
+        player_cards=request.form.get('playerCards').split(',')
+        opp_cards=request.form.get('enemyCards').split(',')
+        print(player_cards)
+        print(opp_cards)
+        with open("card_imgs.csv", "r") as file:
+            card_imgs = file.readlines()
+        card_imgs = [line.strip().split(',') for line in card_imgs]
+
+        player_card_imgs = [card_imgs[int(card)][2] for card in player_cards]
+        opp_card_imgs = [card_imgs[int(card)][2] for card in opp_cards]
+
+        player_cards = [int(card) for card in player_cards]
+        opp_cards = [int(card) for card in opp_cards]
+        player_cards.sort()
+        opp_cards.sort()
+
+        odds = predict_team_odds(player_cards, opp_cards)
+        return render_template("custommatchresults.html", odds=odds, player_cards=player_cards, opp_cards=opp_cards, player_card_imgs=player_card_imgs, opp_card_imgs=opp_card_imgs)
 
 @app.route("/howto", methods=["GET"])
 def howto():
